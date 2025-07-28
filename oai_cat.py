b4ea5e5e0dfdf32534b54dfb3fc81a58947ba153
AramKarimi <aram.karimi@gu.se>
2025-07-28 16:25:32 +0200

    update ariande prefix to load template

--- oai_cat.py
+++ oai_cat.py
@@ -137,7 +137,7 @@ def get_list_records(verb, request, params):
 
     if metadata_prefix == "ksamsok-rdf":
         template = template_ksamsok
-    elif metadata_prefix == "ariadne-rdf":
+    elif metadata_prefix == "shfa-gen-rdf" or metadata_prefix == "ariadne-rdf":
         template = template_ariande
     else:
         template = template_ksamsok  # fallback default
